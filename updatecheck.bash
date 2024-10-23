1d5a2a126a5b0d201ab69487c9622399782fb472
filip2cz <filip@fkomarek.eu>
2024-10-23 22:04:17 +0200

    ADD: autobuild for new TMaze updates

---
+++ updatecheck.bash
@@ -0,0 +1,17 @@
+latest_tag=$(git ls-remote --tags https://github.com/ur-fault/TMaze | awk '{print $2}' | grep -oE "[0-9]+\.[0-9]+\.[0-9]+$" | sort -V | tail -n1)
+
+saved_version=$(cat tmaze.txt)
+
+if [ "$latest_tag" != "$saved_version" ]; then
+    git config --global user.name "github-actions"
+    git config --global user.email "you@example.com"
+    echo "$latest_tag" > tmaze.txt
+    echo "New version $latest_tag found"
+    git add tmaze.txt
+    git commit -m "new version of TMaze: $latest_tag"
+    git push
+    sleep 10
+    gh workflow run build.yml
+else
+    echo "Nothing to do"
+fi
